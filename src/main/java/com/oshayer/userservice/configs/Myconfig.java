6f10761b0f65159c8563aab57c4bcd94ff3929e1
Oshayer-Siddique <oshayer.siddique2001@gmail.com>
2025-07-21 15:19:35 +0600

    test 1

--- src/main/java/com/oshayer/userservice/configs/Myconfig.java
+++ src/main/java/com/oshayer/userservice/configs/Myconfig.java
@@ -1,5 +1,6 @@
 package com.oshayer.userservice.configs;
 
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -7,8 +8,10 @@ import org.springframework.web.client.RestTemplate;
 
 
 @Configuration
+@EnableCaching
 public class Myconfig {
     @Bean
+    @LoadBalanced
 
 
     public RestTemplate restTemplate() {
--- src/main/java/com/oshayer/userservice/services/impl/UserServiceImpl.java
+++ src/main/java/com/oshayer/userservice/services/impl/UserServiceImpl.java
@@ -5,9 +5,13 @@ import com.oshayer.userservice.entites.Rating;
 import com.oshayer.userservice.entites.User;
 import com.oshayer.userservice.repositories.UserRepository;
 import com.oshayer.userservice.services.UserService;
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -28,6 +32,7 @@ public class UserServiceImpl implements UserService {
 
     private Logger logger = LoggerFactory.getLogger(UserServiceImpl.class);
 
+    private static final String RATING_SERVICE_CB = "ratingServiceCB";
 
 
 
@@ -48,6 +53,12 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
+    @Cacheable(value = "users", key = "#id")
+    @CircuitBreaker(name = RATING_SERVICE_CB,fallbackMethod = "getUserByIdFallback")
+    @RateLimiter(name = "userServiceRateLimiter", fallbackMethod = "rateLimiterFallback")
+
+//    @Bulkhead(name = "ratingServiceBH", type = Bulkhead.Type.THREADPOOL)
+
     public User getUserById(Long id) {
         // Fetch user from DB
         User user = userRepository.findById(id).orElse(null);
@@ -57,7 +68,7 @@ public class UserServiceImpl implements UserService {
 
         // ✅ Fetch ratings for this user from rating-service
         List<Rating> ratingsOfUser = restTemplate.exchange(
-                "http://localhost:4030/api/ratings/users/" + user.getId(),
+                "http://RATINGSERVICE/api/ratings/users/" + user.getId(),
                 org.springframework.http.HttpMethod.GET,
                 null,
                 new org.springframework.core.ParameterizedTypeReference<List<Rating>>() {}
@@ -70,7 +81,7 @@ public class UserServiceImpl implements UserService {
             ratingsOfUser = ratingsOfUser.stream().map(rating -> {
                 // Fetch hotel details by hotelId
                 Hotel hotel = restTemplate.getForObject(
-                        "http://localhost:4020/api/hotels/" + rating.getHotelId(),
+                        "http://HOTELSERVICE/api/hotels/" + rating.getHotelId(),
                         Hotel.class
                 );
                 // Set hotel into the rating object
@@ -83,6 +94,26 @@ public class UserServiceImpl implements UserService {
         user.setRatings(ratingsOfUser);
 
         return user;
+    }
+
+    public User getUserByIdFallback(Long id, Throwable throwable) {
+        User fallbackUser = new User();
+        fallbackUser.setId(id);
+        fallbackUser.setName("Fallback User");
+        fallbackUser.setEmail("Fallback email");
+        fallbackUser.setAbout("Service is temporarrily unavailable");
+        fallbackUser.setRatings(new ArrayList<>());
+        return fallbackUser;
+
+    }
+
+    // Fallback method when rate limit is exceeded
+    public User rateLimiterFallback(Long id, Throwable t) {
+        User limited = new User();
+        limited.setId(id);
+        limited.setName("Rate Limit Exceeded");
+        limited.setAbout("Too many requests. Please try again later.");
+        return limited;
     }
 
 }
